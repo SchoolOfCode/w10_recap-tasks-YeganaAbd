cf742b5b47be08a0f0a3d7e5c6864c9cdd4b42f3
YeganaAbd <abd.yegane@gmail.com>
2022-01-31 01:25:15 +0000

    Logout and Profile added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,10 +2,15 @@ import "./App.css";
 import Articles from "../Articles";
 import Header from "../Header";
 import LoginButton from "../Login";
+import LogoutButton from "../Logout";
+import Profile from "../Profile";
+
 function App() {
   return (
     <div className="App">
       <LoginButton />
+      <LogoutButton />
+      <Profile />
       <Header htext="WikiPigeon" />
       <article className="post">
         <Articles />
