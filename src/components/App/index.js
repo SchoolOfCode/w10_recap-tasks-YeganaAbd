89776fb1d3e16d0a801bff32c815c5a4c2b7e77e
YeganaAbd <abd.yegane@gmail.com>
2022-01-31 01:23:11 +0000

    Login added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Articles from "../Articles";
 import Header from "../Header";
-
+import LoginButton from "../Login";
 function App() {
   return (
     <div className="App">
+      <LoginButton />
       <Header htext="WikiPigeon" />
       <article className="post">
         <Articles />
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-1zk4v44o.us.auth0.com"
+      clientId="FTqLA1Fy46Jw6B9AoqiCfZGIWB3v6wXU"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
