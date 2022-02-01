8b9a1562a140a9b4ce04bac5dd5774a18ff65599
YeganaAbd <abd.yegane@gmail.com>
2022-02-01 00:23:01 +0000

    Comment styling

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -1,13 +1,14 @@
 import articles from "../../libs/articles";
 import Btn from "../Button";
 import Comment from "../Comment";
+import styles from "./Articles.module.css";
 
 function Articles() {
   return (
     <div>
       {articles.map((item) => {
         return (
-          <section className="comment-section">
+          <section className={styles.commentSection}>
             <h3>{item.title}</h3>
             <p>{item.paragraphs}</p>
             <Btn text="Like 👍" />
--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,8 +1,9 @@
+import styles from "./Comment.module.css";
 function Comment({ comment }) {
   return (
     <div>
       {" "}
-      <div className="comment">
+      <div className={styles.comment}>
         <h4>{comment.name} says:</h4>
         <p>{comment.text}</p>
       </div>
